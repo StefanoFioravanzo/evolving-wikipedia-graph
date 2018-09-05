bfda43c219559fcb128abaa2047797500a9d1be2
StefanoFioravanzo <stefano.fioravanzo@gmail.com>
2018-09-05 15:27:55 +0200

    Changed docker netowrk name

--- download_output_data.sh
+++ download_output_data.sh
@@ -9,7 +9,7 @@ docker run -it --rm \
     --name hdfs-shell \
     # map local folder output_data
     --volume "`pwd`"/output_data:/output_data \
-    --network sandbox-cluster \
+    --network ewg-cluster \
     -e "CORE_CONF_fs_defaultFS=hdfs://hadoop-namenode:8020" \
     -e "CLUSTER_NAME=hadoop-sandbox" \
     -t uhopper/hadoop:latest \
